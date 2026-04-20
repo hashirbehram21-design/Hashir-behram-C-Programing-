c4f483c5c6819dde2f211ece6df522e62e245206
Hashir Behram <hashirbehram21@gmail.com>
2026-04-20 20:43:38 +0500

    Add files via upload

---
+++ lab 3 task 1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    if(num1 == num2)
+        printf("Both numbers are equal.\n");
+    else if(num1 > num2)
+        printf("First number is greater than second number.\n");
+    else
+        printf("First number is less than second number.\n");
+
+    return 0;
+}
