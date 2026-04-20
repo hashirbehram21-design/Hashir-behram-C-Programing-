8f2413e58ade0869ff1668071d3180c780646955
Hashir Behram <hashirbehram21@gmail.com>
2026-04-20 20:45:24 +0500

    Add files via upload

---
+++ lab task 5.cpp
@@ -0,0 +1,23 @@
+//Sequential 
+//print your name 10 times 
+
+#include <iostream>
+using namespace std;
+
+int main()
+	{
+	
+	
+	cout<<" Hashir Behram "<<endl;
+	cout<<" Hashir Behram "<<endl;
+	cout<<" Hashir Behram "<<endl;
+	cout<<" Hashir Behram "<<endl;
+	cout<<" Hashir Bebram "<<endl;
+	cout<<" Hashir Behram "<<endl;
+	cout<<" Hashir Behram "<<endl;
+	cout<<" Hashir Behram "<<endl;
+	cout<<" Hashir Behram "<<endl;
+	cout<<" Hashir Behram "<<endl;	
+		
+	return 0;}
+
