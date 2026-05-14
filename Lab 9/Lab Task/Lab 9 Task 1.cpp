196e1be08a05fa8ffc232060032ae1f0022fc56a
Hashir Behram <hashirbehram21@gmail.com>
2026-05-14 09:32:11 +0500

    Add files via upload

---
+++ Lab 9/Lab Task/Lab 9 Task 1.cpp
@@ -0,0 +1,16 @@
+// Name: Hashir Behram
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0747
+// Section: A
+
+#include <iostream>
+using namespace std;
+
+void greet() {
+    cout << "Welcome to Programming Lab!" << endl;
+}
+
+int main() {
+    greet();  // function call
+    return 0;
+}
---
+++ Lab 9/Lab Task/Lab 9 Task 2.cpp
@@ -0,0 +1,15 @@
+// Name: Hashir Behram
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0747
+// Section: A
+#include <iostream>
+using namespace std;
+
+void sayHello() {
+    cout << "Hello, World!" << endl;
+}
+
+int main() {
+    sayHello();  // function call
+    return 0;
+}
